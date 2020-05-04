b07b1c6dadac90dd763ec3463041fa370efb791e
Jeremy Bouquain <j.bouq@wanadoo.fr>
2020-05-04 02:10:41 +0200

    Add ability to check if email is blacklisted

--- src/UtagawaVtt/PhpListRESTAPIClient.php
+++ src/UtagawaVtt/PhpListRESTAPIClient.php
@@ -359,6 +359,22 @@ class PhpListRESTAPIClient
         return $this->checkValidity($result);
     }
 
+    /**
+     * Check if subscriber email is blacklisted
+     *
+     * @param string $emailAddress email address of the subscriber to remove from blacklist
+     *
+     * @return true if blacklisted, false if not or on error
+     */
+    public function subscriberIsBlacklisted($emailAddress)
+    {
+        $post_params = array(
+            'email' => $emailAddress,
+        );
+        $result = $this->callAPI('blacklistedEmailInfo', $post_params);
+        return (isset($result->status) && $result->status === 'success' && $result->type === "blacklist");
+    }
+
      /**
       * Get the total number of subscribers.
       *
